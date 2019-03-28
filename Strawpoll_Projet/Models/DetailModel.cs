d55d63fa63329638478c2f71aea63174510f5b1b
Amadou Bailo BAH <coladaitp18-bam@ccicampus.fr>
2019-03-28 11:30:19 +0100

    Modification propriete

--- Strawpoll_Projet/Models/DetailModel.cs
+++ Strawpoll_Projet/Models/DetailModel.cs
@@ -8,12 +8,12 @@ namespace Strawpoll_Projet.Models
     public class DetailModel
     {
         public string Question { get; private set; }
-        public string Reponse1;
-        public string Reponse2;
-        public string Reponse3;
-        public bool ChoixMultiple;
-        public int 
-        
+        public string Reponse1 { get; private set; }
+        public string Reponse2 { get; private set; }
+        public string Reponse3 { get; private set; }
+        public bool ChoixMultiple { get; private set; }
+        public int votant { get; private set; }
+
     }
 
 }
